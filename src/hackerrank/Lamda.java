04e195a344d4f7ee44ced373ee6097c159b81573
kakaict <canhcung88@gmail.com>
2017-09-10 11:03:04 +0200

    hackerrank

---
+++ src/hackerrank/Lamda.java
@@ -0,0 +1,70 @@
+package hackerrank;
+
+
+/**
+ *
+ *khaitq on 10 sept. 2017
+ */
+
+
+interface PerformOperation {
+	 boolean check(int a);
+}
+
+class MyMath {
+	 public static boolean checker(PerformOperation p, int num) {
+		 return p.check(num);
+	 }
+	 
+	 public PerformOperation isOdd(){
+		return new PerformOperation() {
+			@Override
+			public boolean check(int a) {
+		          if (a%2 == 0) 
+		              return false;
+		          return true;
+			}
+		};
+	 }
+	 
+	 public PerformOperation isPrime(){
+		 return new PerformOperation() {
+			@Override
+			public boolean check(int a) {
+				int t = a/2;
+		          for (int i = 0; i < t; i++){
+		              if (a%i ==0){
+		                 return false;
+		              }
+		          }
+		          return true;
+			}
+		};
+	 }
+	 
+	 public PerformOperation isPalindrome(){
+		    return new PerformOperation() {
+				@Override
+				public boolean check(int a) {
+					int palindrome = a;
+		            int reverse = 0;
+
+		            while (palindrome != 0) {
+		                int remainder = palindrome % 10;
+		                reverse = reverse * 10 + remainder;
+		                palindrome = palindrome / 10;
+		            }
+
+		            return a == reverse; 
+				}
+			};
+	 }
+}
+
+public class Lamda {
+	
+	
+
+}
+
+
