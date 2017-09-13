4cf7807912a00928f14ea421479d8efaf1e5daad
kakaict <canhcung88@gmail.com>
2017-09-14 00:43:12 +0200

    init

---
+++ src/creackingCode/recusive/StringPermutation.java
@@ -0,0 +1,25 @@
+package creackingCode.recusive;
+
+
+/**
+ *
+ *khaitq on 14 sept. 2017
+ */
+
+
+/*
+ * ABC -> ACB, BCA, BAC, CAB, CBA
+ * 
+ * AB -> BA
+ */
+
+public class StringPermutation {
+	
+	public static void main(String[] args){
+		
+		
+	}
+
+}
+
+
