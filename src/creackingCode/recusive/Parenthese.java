335bde0806b14e95d7896ad11417e0d1336aadb1
kakaict <canhcung88@gmail.com>
2017-09-16 00:49:56 +0200

    update

---
+++ src/creackingCode/recusive/Parenthese.java
@@ -0,0 +1,52 @@
+package creackingCode.recusive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * khaitq on 16 sept. 2017
+ * 
+ * 
+ * Implement an algorithm to print all valid (e.g., properly opened and closed) combinations of n-pairs of parentheses.
+ * EXAMPLE:
+ * input: 3 (3 pairs of parentheses)
+ * output: ()()(), ()(()), (())(), ((()))   (()())
+ * 
+ * 2 paires => ()(), (())
+ */
+
+public class Parenthese {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getParenthese(2));
+		
+		
+	}
+
+	private static final String unit = "()";
+	
+	public static List<String> getParenthese(int n) {
+		
+		List<String> r = new ArrayList<String>();
+		if(n ==1) {
+			r.add(unit );
+		}
+		
+		if(n==2) {
+			r.add(unit+unit);
+			r.add("(" + unit + ")");
+		}
+		
+		if (n  > 2) {
+			//TODO
+			
+		}
+		
+		return r;
+	}
+
+}
+
+
