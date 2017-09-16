34e139c499b1d7f7e5a84d489be9667ac4f2c32e
kakaict <canhcung88@gmail.com>
2017-09-16 14:06:02 +0200

    update

--- src/creackingCode/recusive/DuplicateChar.java
+++ src/creackingCode/recusive/DuplicateChar.java
@@ -4,6 +4,12 @@ package creackingCode.recusive;
 /**
  *
  *khaitq on 16 sept. 2017
+ *
+ *Find duplicate character in a String, do not use an extra String, Buffer, Collection
+ *
+ * Note : char -> int (ascii value)   0 - 256 for all characters
+ * 
+ * -> can use an array of boolean to present all characters
  */
 
 public class DuplicateChar {
@@ -12,6 +18,9 @@ public class DuplicateChar {
 
 		
 		System.out.println(containsDuplicate("ABCDA"));
+		
+		System.out.println(containsDuplicate("ABCDEFGH"));
+		
 	}
 	
 	public static boolean containsDuplicate(String s) {
