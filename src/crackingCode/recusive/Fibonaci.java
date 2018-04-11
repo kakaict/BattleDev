bb4e4c985ef4f3600fd2f8b9f619471ed3855fa2
kakaict <canhcung88@gmail.com>
2018-04-11 20:16:49 +0200

    update

--- src/crackingCode/recusive/Fibonaci.java
+++ src/crackingCode/recusive/Fibonaci.java
@@ -16,6 +16,8 @@ public class Fibonaci {
 		System.out.println(fin(10));
 		
 		System.out.println(finLink(10));
+		
+		System.out.println(find(10));
 	}
 	
 	public static int fin(int n){
@@ -23,9 +25,28 @@ public class Fibonaci {
 		int r = 0;
 		
 		if (n == 1) return 1;
-		if (n == 2) return 1;
+		if (n == 2) return 2;
 		
 		r = fin(n-1) + fin (n-2);
+		
+		return r;
+	}
+	
+	public static int find(int n) {
+		int r = 0;
+		
+		if (n == 1) return 1;
+		
+		if (n == 2) return 2;
+		
+		int n1 = 1;
+		int n2 = 2;
+		
+		for (int i = 2; i < n ; i++) {
+			r = n1 + n2;
+			n1 = n2;
+			n2 = r;
+		}
 		
 		return r;
 	}
@@ -37,7 +58,7 @@ public class Fibonaci {
 		for (int i = 0; i  < n; i ++) {
 			
 			if (i == 0) list.add(1);
-			if (i == 1) list.add(1); 
+			if (i == 1) list.add(2); 
 			
 			if (i  > 1) {
 				list.add(list.get(i-1) + list.get(i-2));
